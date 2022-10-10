3cadb79e4d02f0d9139f0b676e06be0ff4fd0e47
Carlos03s <carlosq12k@gmail.com>
2022-10-10 00:23:50 -0300

    .

--- src/database/schemas/schema-user.sql
+++ src/database/schemas/schema-user.sql
@@ -8,3 +8,15 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `email` varchar(100) UNIQUE NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+-- user cateogry, selected after first log in
+CREATE TABLE IF NOT EXISTS `usuario_category` (
+   `id` int(255) NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(120) NOT NULL,
+  `sub_categoria` varchar(255) NOT NULL,
+  `sub_categoria2` varchar(255),
+  `sub_categoria3` varchar(255),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_curso`) REFERENCES curso(id) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
